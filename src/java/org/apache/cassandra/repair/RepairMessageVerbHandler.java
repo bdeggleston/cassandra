b7010722146ff246e08e2211903c521a122e4be1
Blake Eggleston <bdeggleston@gmail.com>
2016-10-24 15:52:37 -0700

    add local consistent repair logic

--- src/java/org/apache/cassandra/repair/RepairMessageVerbHandler.java
+++ src/java/org/apache/cassandra/repair/RepairMessageVerbHandler.java
@@ -122,6 +122,7 @@ public class RepairMessageVerbHandler implements IVerbHandler<RepairMessage>
                         return;
                     }
 
+                    ActiveRepairService.instance.consistent.local.maybeSetRepairing(desc.parentSessionId);
                     Validator validator = new Validator(desc, message.from, validationRequest.gcBefore);
                     CompactionManager.instance.submitValidation(store, validator);
                     break;
