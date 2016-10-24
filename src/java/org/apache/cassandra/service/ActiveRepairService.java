26156bbca9351957d2be708c10c8658d7603d21d
Blake Eggleston <bdeggleston@gmail.com>
2016-10-24 15:52:37 -0700

    add local consistent repair logic

--- src/java/org/apache/cassandra/service/ActiveRepairService.java
+++ src/java/org/apache/cassandra/service/ActiveRepairService.java
@@ -17,7 +17,6 @@
  */
 package org.apache.cassandra.service;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -28,6 +27,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Futures;
@@ -688,6 +688,16 @@ public class ActiveRepairService implements IEndpointStateChangeSubscriber, IFai
         public Collection<ColumnFamilyStore> getColumnFamilyStores()
         {
             return ImmutableSet.<ColumnFamilyStore>builder().addAll(columnFamilyStores.values()).build();
+        }
+
+        public Set<UUID> getCfIds()
+        {
+            return ImmutableSet.copyOf(Iterables.transform(getColumnFamilyStores(), cfs -> cfs.metadata.cfId));
+        }
+
+        public Collection<Range<Token>> getRanges()
+        {
+            return ImmutableSet.copyOf(ranges);
         }
 
         @Override
