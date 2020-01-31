f3af56e5a8bbc42e28505140046689b097de1c49
Blake Eggleston <bdeggleston@gmail.com>
2020-01-30 17:05:47 -0800

    initial work on benchmark visualization

---
+++ test/bin/perf-visualize.py
@@ -0,0 +1,182 @@
+import argparse
+import json
+import sys
+
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from matplotlib.axes import Axes
+from matplotlib.colors import Normalize, LogNorm, SymLogNorm
+from matplotlib.image import AxesImage
+from matplotlib import gridspec
+
+import numpy as np
+
+parser = argparse.ArgumentParser(description="visualize jmh json output")
+parser.add_argument('--prettify', '-p', dest='prettify', action='store_true', help="don't process anything, just rewrite the given files in an indented format")
+parser.add_argument('files', metavar='F', nargs='+', help='files to visualize')
+
+args = parser.parse_args()
+
+if args.prettify:
+    for fname in args.files:
+        with open(fname, 'r') as f:
+            js = json.load(f)
+        with open(fname, 'w') as f:
+            json.dump(js, f, indent=4, sort_keys=True)
+    sys.exit(0)
+
+class Trial(object):
+    def __init__(self, params: dict, expanded=None):
+        self.params = params
+        self.expanded = expanded or dict()
+        self.results = []
+
+    def add_result(self, metrics):
+        result = {'score': metrics['score'],
+                  '50.0': metrics['scorePercentiles']['50.0'],
+                  '99.9': metrics['scorePercentiles']['99.9']}
+        self.results.append(result)
+
+
+class Benchmark(object):
+    def __init__(self, name: str):
+        self.name = name
+        self.trials = {}
+
+class ValueSet(object):
+    def __init__(self, name):
+        self.name = name
+        self.values = set()
+
+    def add(self, value):
+        self.values.add(value)
+
+    def sorted(self):
+        if all([str.isnumeric(v.replace('-', '')) for v in self.values]):
+            is_float = any(['.' in v for v in self.values])
+            cmp = (lambda v: float(v)) if is_float else (lambda v: int(v))
+            return sorted(self.values, key=cmp)
+        else:
+            return sorted(self.values)
+
+    def tuple(self):
+        assert len(self.values) == 1
+        return (self.name, list(self.values)[0])
+
+benchmarks = {}
+
+x_param = 'concentration'
+y_params = ['columnCount', 'rowOverlap', 'distribution']
+unlisted_params = []
+results = ['score', '50.0', '99.9']
+param_values = {}
+
+def is_listed_dimension(dim):
+    return x_param == dim or dim in y_params
+
+# parse and organize the data
+
+for fname in args.files:
+    with open(fname, 'r') as f:
+        js = json.load(f)
+
+    for row in js:
+        bench_name = row['benchmark']
+        benchmark = benchmarks.setdefault(bench_name, Benchmark(bench_name))
+        params: dict = row['params']
+        key = frozenset(params.items())
+
+        trial = benchmark.trials.setdefault(key, Trial(params, row['expandedParams']))
+        trial.add_result(row['primaryMetric'])
+
+        for k, v in params.items():
+            if k not in param_values:
+                param_values[k] = ValueSet(k)
+
+            vset = param_values[k]
+            vset.add(v)
+
+# https://matplotlib.org/3.1.0/gallery/images_contours_and_fields/image_annotated_heatmap.html
+
+# check that there aren't any unlisted dimensions with multiple values
+for k, v in param_values.items():
+    if not is_listed_dimension(k):
+        if len(v.values) > 1:
+            print(k, "has multiple values and doesn't have a graphing dimension")
+            sys.exit(1)
+        else:
+            unlisted_params.append(k)
+
+
+
+def make_row_map(params, previous=None):
+    if not params:
+        return previous
+    param = params[0]
+    to_append = [(param, v) for v in param_values[param].sorted()]
+    if previous is None:
+        return make_row_map(params[1:], [[v] for v in to_append])
+    else:
+        appended = []
+        for row in previous:
+            for value in to_append:
+                appended.append(row + [value])
+
+        return make_row_map(params[1:], appended)
+
+row_labels = make_row_map(y_params)
+
+data_name = "99.9"
+
+rows = []
+for bench_name, benchmark in benchmarks.items():
+    for label in row_labels:
+        columns = []
+        for value in param_values[x_param].sorted():
+            key = set(label)
+            key.add((x_param, value))
+            for unlisted_param in unlisted_params:
+                key.add(param_values[unlisted_param].tuple())
+
+            key = frozenset(key)
+            if key not in benchmark.trials:
+                columns.append(0)
+                continue
+            trial = benchmark.trials[frozenset(key)]
+            columns.append(trial.results[0][data_name])
+        rows.append(np.array(columns))
+rows = np.array(rows)
+
+def heatmap(data: np.ndarray, ax):
+    im: AxesImage = ax.imshow(data, cmap=cm.rainbow,
+                              norm=Normalize(vmin=data.min(), vmax=data.max(), clip=True))
+    return im
+
+def make_color_bar(im, ax, label):
+    cbar = ax.figure.colorbar(im, ax=ax)
+    return cbar
+
+def annotate(data, ax, name, labels):
+    ax.set_title("{} [{:.4f} - {:.4f}]".format(name, data.min(), data.max()))
+    ax.set_yticks(np.arange(len(labels)))
+    ax.set_yticklabels(["{}/{:.1f}/{}".format(r[0][1], float(r[1][1]), r[2][1][0])
+                        for r in labels])
+
+class BenchmarkViz(object):
+    def __init__(self, benchmark: Benchmark):
+        self.benchmark = benchmark
+
+fig_name = data_name + " ops/ms"
+fig = plt.figure(figsize=[4, 6])
+split = 4
+spec = gridspec.GridSpec(ncols=1, nrows=split, figure=fig)
+rr = 0
+for (data, labels) in zip(np.split(rows, split), np.split(np.array(row_labels), split)):
+
+    ax = fig.add_subplot(spec[rr, 0])
+    im = heatmap(data, ax)
+    annotate(data, ax, fig_name, labels)
+    cbar = make_color_bar(im, ax, data_name)
+    rr += 1
+
+plt.show()
