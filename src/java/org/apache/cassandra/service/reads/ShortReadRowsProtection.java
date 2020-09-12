f4799f9733e5e13baf64443b2094f55067aa7dee
Blake Eggleston <bdeggleston@gmail.com>
2020-09-12 16:36:55 -0700

    fix consistency dtest

--- src/java/org/apache/cassandra/service/reads/ShortReadRowsProtection.java
+++ src/java/org/apache/cassandra/service/reads/ShortReadRowsProtection.java
@@ -118,7 +118,7 @@ class ShortReadRowsProtection extends Transformation implements MoreRows<Unfilte
          * This is a table with no clustering columns, and has at most one row per partition - with EMPTY clustering.
          * We already have the row, so there is no point in asking for more from the partition.
          */
-        if (Clustering.EMPTY == lastClustering)
+        if (lastClustering.isEmpty())
             return null;
 
         lastFetched = singleResultCounter.rowsCountedInCurrentPartition() - lastCounted;
